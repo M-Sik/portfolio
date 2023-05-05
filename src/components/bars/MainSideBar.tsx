8c4ba521afbf2944d23adf36313c5ffe7738a16a
김명식 <rlaaudtlr233@naver.com>
2023-05-05 19:15:32 +0900

    [design] main sideBar 호버시 애니메이션 적용

--- src/components/bars/MainSideBar.tsx
+++ src/components/bars/MainSideBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Styles from './MainSideBar.module.scss';
 
 export default function MainSideBar() {
   return (
     <aside className=" bg-neutral-900 flex flex-col justify-between px-6 py-14">
       <div className="flex flex-col gap-5">
         <a
-          className=" font-bold text-3xl text-white"
+          className={`font-bold text-3xl text-white p-2 rounded-lg ${Styles.text_hover}`}
           href="https://github.com/M-Sik"
           target="_blank"
           aria-label="github link"
@@ -13,7 +14,7 @@ export default function MainSideBar() {
           GitHub <span className="text-yellow-400">.</span>
         </a>
         <a
-          className=" font-bold text-3xl text-white"
+          className={`font-bold text-3xl text-white p-2 rounded-lg ${Styles.text_hover}`}
           href="https://sikk.tistory.com/"
           target="_blank"
           aria-label="blog link"
