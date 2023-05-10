0673d98fd37672d5e0cf6bdebaec79bee28ad166
김명식 <rlaaudtlr233@naver.com>
2023-05-10 21:05:34 +0900

    [feat] project aboutme 상세내용 작성

--- src/app/projects/about-me/page.tsx
+++ src/app/projects/about-me/page.tsx
@@ -1,5 +1,86 @@
-import React from 'react';
+'use client';
+
+import BottomToTopFadeIn from '@/components/gasp/BottomToTopFadeIn';
+import FadeIn from '@/components/gasp/FadeIn';
+import Underline from '@/components/gasp/UnderLine';
+import React, { useEffect } from 'react';
+import Image from 'next/image';
+
+const links = [
+  {
+    url: 'https://devsik-aboutme.netlify.app',
+    label: 'about me web link',
+    text: '👉 웹 보러가기 👈',
+  },
+  {
+    url: 'https://github.com/M-Sik/aboutMe',
+    label: 'diary github link',
+    text: '👉 깃허브 보러가기 👈',
+  },
+];
+const skills = ['Vue.js', 'Javascript', 'Scss', 'Vuetify', 'Git', 'Github'];
 
 export default function AboutMePage() {
-  return <div>AboutMePage</div>;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <section className="w-full">
+      <FadeIn delay={0.3}>
+        <Underline delay={0.5} duration={2} color="rgb(248, 205, 7)" className="w-full block">
+          <h1 className=" text-4xl font-bold">AboutMe</h1>
+        </Underline>
+      </FadeIn>
+      <FadeIn delay={0.6}>
+        <p className="mt-4">
+          About Me는 저를 소개하기 위한 웹페이지입니다. Vue.js, Scss, 반응형 웹의 숙련도를 올리고자
+          제작하였습니다.
+        </p>
+      </FadeIn>
+      <FadeIn delay={0.9}>
+        <h2 className={subTitleStyle}>🔗 서비스 링크</h2>
+        <div className="flex mt-2">
+          <ul className="flex flex-col bg-gray-100 rounded-xl p-4">
+            {links.map(({ url, text, label }) => (
+              <li key={url} className="py-1 flex">
+                <a href={url} target="_blank" aria-label={label} className={linkStyle}>
+                  {text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </FadeIn>
+      <FadeIn delay={1.2}>
+        <h2 className={subTitleStyle}>🛠️ 사용 기술</h2>
+        <div className="w-full flex gap-2 flex-wrap mt-2">
+          {skills.map((skill) => (
+            <p key={skill} className="py-1 px-2 bg-gray-200 rounded-md text-purple-600 font-bold">
+              {skill}
+            </p>
+          ))}
+        </div>
+      </FadeIn>
+      <FadeIn delay={1.5}>
+        <h2 className={subTitleStyle}>💻 작업 내용</h2>
+        <ul className={listStyle}>
+          <li>About Me 기획 및 디자인</li>
+        </ul>
+        <ul className={listStyle}>
+          <li>반응형 UI 구현</li>
+        </ul>
+      </FadeIn>
+      <FadeIn delay={1.8}>
+        <h2 className={subTitleStyle}>👨‍👨‍👦 참가 인원</h2>
+        <ul className={listStyle}>
+          <li>개인 프로젝트</li>
+        </ul>
+      </FadeIn>
+    </section>
+  );
 }
+
+const subTitleStyle = 'mt-16 mobile:mt-8 text-2xl font-bold';
+const listStyle = 'mt-2 list-disc pl-6';
+const linkStyle = 'hover:text-yellow-400 hover:font-bold';
