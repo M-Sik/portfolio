c9a056fd17735d06d38662a95e2851ced8a94435
김명식 <rlaaudtlr233@naver.com>
2024-03-03 15:09:02 +0900

    [refactor] 메인화면 목차 사이드바 시멘틱 태그 적용 및 반복태그 및 반복함수 제거

--- src/components/bars/SideNavBar.tsx
+++ src/components/bars/SideNavBar.tsx
@@ -1,47 +1,45 @@
 import React, { useEffect } from 'react';
 
 type Props = {
-  highlight: 'skill' | 'career' | 'project';
+  highlight: NavItem;
 };
+type NavItem = 'skill' | 'career' | 'project';
 
 export default function SideNavBar({ highlight }: Props) {
-  const scrollSkill = () => {
-    document.getElementById('skillSection')?.scrollIntoView({ block: 'start', behavior: 'smooth' });
-  };
-  const scrollCareer = () => {
-    document
-      .getElementById('careerSection')
-      ?.scrollIntoView({ block: 'start', behavior: 'smooth' });
-  };
-  const scrollProject = () => {
-    document
-      .getElementById('projectSection')
-      ?.scrollIntoView({ block: 'start', behavior: 'smooth' });
+  const navItems: { name: string; value: NavItem }[] = [
+    { name: '🛠️ 기술 스택', value: 'skill' },
+    { name: '💼 경력', value: 'career' },
+    { name: '💻 개인 프로젝트', value: 'project' },
+  ];
+  const handleClick = (value: NavItem) => {
+    if (value === 'skill')
+      return document
+        .getElementById('skillSection')
+        ?.scrollIntoView({ block: 'start', behavior: 'smooth' });
+    else if (value === 'career')
+      return document
+        .getElementById('careerSection')
+        ?.scrollIntoView({ block: 'start', behavior: 'smooth' });
+    else if (value === 'project')
+      return document
+        .getElementById('projectSection')
+        ?.scrollIntoView({ block: 'start', behavior: 'smooth' });
   };
   return (
-    <article className={`h-full hidden xl:block absolute top-0 left-[4vw]`}>
+    <aside className={`h-full hidden xl:block absolute top-0 left-[4vw]`}>
       <nav className=" sticky top-0 pt-[150px]">
         <ul className="flex flex-col gap-8 border-l-8 border-yellow-400 pl-4 font-bold text-xl text-gray-500 cursor-pointer">
-          <li
-            onClick={scrollSkill}
-            className={`hover:text-black ${highlight === 'skill' && 'text-yellow-400'}`}
-          >
-            🛠️ 기술 스택
-          </li>
-          <li
-            onClick={scrollCareer}
-            className={`hover:text-black ${highlight === 'career' && 'text-yellow-400'}`}
-          >
-            💼 경력
-          </li>
-          <li
-            onClick={scrollProject}
-            className={`hover:text-black ${highlight === 'project' && 'text-yellow-400'}`}
-          >
-            💻 개인 프로젝트
-          </li>
+          {navItems.map(({ name, value }, i) => (
+            <li
+              key={i}
+              className={`hover:text-black ${highlight === value && 'text-yellow-400'}`}
+              onClick={() => handleClick(value)}
+            >
+              {name}
+            </li>
+          ))}
         </ul>
       </nav>
-    </article>
+    </aside>
   );
 }
