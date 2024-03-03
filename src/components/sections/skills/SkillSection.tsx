66d2099d2412c74adfb90146028630acfc4c62fd
김명식 <rlaaudtlr233@naver.com>
2024-03-03 15:16:27 +0900

    [refactor] 기술스택 섹션 컴포넌트 시멘틱 태그 적용으로 인한 태그 수정

--- src/components/sections/skills/SkillSection.tsx
+++ src/components/sections/skills/SkillSection.tsx
@@ -32,37 +32,43 @@ type Props = {
 export default function SkillSection({ id }: Props) {
   return (
     <section id={id} className="w-full bg-white lg:pt-[150px] relative">
-      <article className=" max-w-[800px] mx-auto mobile:px-4 mobile:py-7">
-        <BottomToTopFadeIn delay={0}>
-          <Underline
-            delay={1}
-            duration={2}
-            color="rgb(248, 205, 7)"
-            className=" text-2xl w-full block"
-          >
-            <h1 className="font-bold text-neutral-700">🛠️ 기술 스택</h1>
-          </Underline>
-        </BottomToTopFadeIn>
-        <section className="mt-6">
-          {skills.map(({ title, contents }) => (
-            <BottomToTopFadeIn key={title} delay={0}>
-              <article className="flex gap-8 mobile:block pb-3 items-center">
-                <div className=" w-[200px] text-gray-600">{title}</div>
-                <div className="flex gap-3">
-                  {contents.map((content) => (
-                    <p
-                      key={content}
-                      className="py-1 px-2 bg-gray-200 rounded-md text-purple-600 font-bold"
-                    >
-                      {content}
-                    </p>
-                  ))}
-                </div>
-              </article>
-            </BottomToTopFadeIn>
-          ))}
-        </section>
-      </article>
+      <dl className=" max-w-[800px] mx-auto mobile:px-4 mobile:py-7">
+        <dt>
+          <BottomToTopFadeIn delay={0}>
+            <Underline
+              delay={1}
+              duration={2}
+              color="rgb(248, 205, 7)"
+              className=" text-2xl w-full block"
+            >
+              <p className="font-bold text-neutral-700">🛠️ 기술 스택</p>
+            </Underline>
+          </BottomToTopFadeIn>
+        </dt>
+        <dd className="mt-6">
+          <ul>
+            {skills.map(({ title, contents }) => (
+              <li key={title}>
+                <BottomToTopFadeIn delay={0}>
+                  <dl className="flex gap-8 mobile:block pb-3 items-center">
+                    <dt className=" w-[200px] text-gray-600">{title}</dt>
+                    <dd className="flex gap-3">
+                      {contents.map((content) => (
+                        <p
+                          key={content}
+                          className="py-1 px-2 bg-gray-200 rounded-md text-purple-600 font-bold"
+                        >
+                          {content}
+                        </p>
+                      ))}
+                    </dd>
+                  </dl>
+                </BottomToTopFadeIn>
+              </li>
+            ))}
+          </ul>
+        </dd>
+      </dl>
     </section>
   );
 }
